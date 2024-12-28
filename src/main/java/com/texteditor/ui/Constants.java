bf9f9034d15969c6ff4db91b09b1e5c6fd1c62e2
Kibrewossen <47031138+kebtes@users.noreply.github.com>
2024-12-28 22:16:15 +0300

    Modified class

---
+++ src/main/java/com/texteditor/ui/Constants.java
@@ -0,0 +1,15 @@
+package com.texteditor.ui;
+
+import javafx.scene.paint.Color;
+
+public class Constants {
+    public static Color BACKGROUND_COLOR = Color.web("252D38");
+    public static Color FOOTER_COLOR = Color.web("14191F");
+    public static Color FONT_WHITE = Color.WHITE;
+
+    public static String ICON_PATH = "Text Editor\\src\\main\\java\\com\\texteditor\\ui\\assets\\icon.png";
+
+    public static String getCssColor(Color color) {
+        return String.format("#%02X%02X%02X", (int)(color.getRed() * 255), (int)(color.getGreen() * 255), (int)(color.getBlue() * 255));
+    }
+}
